a67c69d45876a6819b4e46f562cc5d9a1341fbc6
TembaNkabi <mrnkabi@gmail.com>
2024-03-30 06:48:54 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# A Bash script that takes in a URL, sends a GET request and displays only the  body of a 200 status code response
+curl -Ls "$1"
